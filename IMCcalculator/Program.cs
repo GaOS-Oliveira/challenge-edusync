8372910f33d1d827af0d790bd1b18e795a632f84
Gabriel Oliveira <gabrieloliveira.gos04@gmail.com>
2022-06-14 12:01:34 -0300

    Calculator ready

--- IMCcalculator/Program.cs
+++ IMCcalculator/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using IMCcalculatorLibraries;
+
+static void Diagnostic()
+{
+    Console.WriteLine("**********************");
+    Console.WriteLine("DIAGNÓSTICO PRÉVIO");
+
+    var calc = new Calculator();
+    
+    Console.Write("\nNome: ");
+    string name = Console.ReadLine();
+
+    Console.Write("Sexo (Masculino ou Feminino): ");
+    string sex = Console.ReadLine();
+
+    Console.Write("Idade: ");
+    int age = int.Parse(Console.ReadLine());
+
+    Console.Write("Altura (em centimetros [cm]): ");
+    double height = double.Parse(Console.ReadLine());
+
+    Console.Write("Peso (em quilogramas [Kg]): ");
+    double weight = double.Parse(Console.ReadLine());
+
+    string category = calc.SelectCategory(age);
+    Console.WriteLine($"Categoria: {category}");
+
+    Console.WriteLine($"\nIMC Desejável: entre 20 e 24");
+
+    double imc = calc.CalculateIMC(weight, height);
+    Console.WriteLine($"\nResultado IMC: {imc:0.00}");
+
+    string risk = calc.IndicateRisks(imc);
+    Console.WriteLine($"\nRiscos: {risk}");
+
+    string recommendation = calc.RecommendSolution(imc);
+    Console.WriteLine($"\nRecomendação inicial: {recommendation}");
+
+    Console.WriteLine("\n**********************");
+}
+
+Diagnostic();
---
+++ IMCcalculatorLibraries/Calculator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMCcalculatorLibraries
+{
+    public class Calculator
+    {
+        public string SelectCategory(int age)
+        {
+            string category = "";
+
+            switch (age)
+            {
+                case < 12:
+                    category = "Infantil";
+                    break;
+                case < 21:
+                    category = "Juvenil";
+                    break;
+                case < 66:
+                    category = "Adulto";
+                    break;
+                case >= 66:
+                    category = "Idoso";
+                    break;
+            }
+
+            return category;
+        }
+
+        public double CalculateIMC(double weight, double height)
+        {
+            double imc = weight / (Math.Pow(height/100, 2.0));
+            return imc;
+        }
+
+        public string IndicateRisks(double imc)
+        {
+            string risks = "";
+
+            switch (imc)
+            {
+                case < 20:
+                    risks = "Muitas complicações de saúde como doenças pulmonares e cardiovasculares podem estar associadas ao baixo peso.";
+                    break;
+                case < 25:
+                    risks = "Seu peso está ideal para suas referências.";
+                    break;
+                case < 30:
+                    risks = "Aumento de peso apresenta risco moderado para outras doenças crônicas e cardiovasculares.";
+                    break;
+                case < 36:
+                    risks = "Quem tem obesidade vai estar mais exposto a doenças graves e ao risco de mortalidade.";
+                    break;
+                case >= 36:
+                    risks = "O obeso mórbido vive menos, tem alto risco de mortalidade geral por diversas causas.";
+                    break;
+            }
+
+            return risks;
+        }
+
+        public string RecommendSolution(double imc)
+        {
+            string recommendation = "";
+
+            switch (imc)
+            {
+                case < 20:
+                    recommendation = "Inclua carboidratos simples em sua dieta, além de proteínas - indispensáveis para ganho de massa magra. Procure um profissional.";
+                    break;
+                case < 25:
+                    recommendation = "Mantenha uma dieta saudável e faça exames periódicos.";
+                    break;
+                case < 30:
+                    recommendation = "Adote um tratamento baseado em dieta balanceada, exercício físico e medicação. A ajuda de um profissional pode ser interessante.";
+                    break;
+                case < 36:
+                    recommendation = "Adote uma dieta alimentar rigorosa, com o acompanhamento de um nutricionista e um médico especialista (endócrino).";
+                    break;
+                case >= 36:
+                    recommendation = "Procure com urgência o acompanhamento de um nutricionista para realizar reeducação alimentar, um psicólogo e um médico especialista (endócrino).";
+                    break;
+            }
+
+            return recommendation;
+        }
+    }
+}
