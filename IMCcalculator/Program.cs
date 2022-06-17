3098fd908d96bc8cc6ae5090893024b32528a601
Gabriel Oliveira <gabrieloliveira.gos04@gmail.com>
2022-06-17 10:09:36 -0300

    New function to clean the code

--- IMCcalculator/Program.cs
+++ IMCcalculator/Program.cs
@@ -1,145 +1,28 @@
 ﻿using IMCcalculatorLibraries;
 using System.Globalization;
 
-static void Diagnostic()
+static void getDiagnostic()
 {
+    var calc = new Calculator();
+
+    var print = new PrintLibrary();
+
     Console.WriteLine("**********************");
     Console.WriteLine("DIAGNÓSTICO PRÉVIO\n");
 
-    // Variável de objeto contendo as funções para cálculo do IMC:
-    var calc = new Calculator();
+    string name = print.getName();
 
-    // Variável para validação de valores das outras variáveis:
-    bool validation = true;
-
-    // TextInfo var para formatação de texto:
-    TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
-
-    #region "Variavel 'name'"
-    string name = "";
-    while (validation)
-    {
-        Console.Write("Nome: ");
-        var name_temp = Console.ReadLine();
-
-        // Correção da Exceção CS8600
-        if (name_temp is not null && name_temp.Length > 0)
-        {
-            name = name_temp;
-            validation = false; // Se o valor for válido
-        }
-    }
-
-    name = textInfo.ToTitleCase(name.ToLower());
-    #endregion
-
-    #region "Variável 'sex'"
-    validation = true;
-    string sex = "";
-    while (validation)
-    {
-        Console.Write("Sexo (Masculino ou Feminino): ");
-        var sex_temp = Console.ReadLine();
-
-        // Correção da Exceção CS8600
-        if (sex_temp is not null)
-        {
-            sex = sex_temp.ToLower();
-        }
-
-        if (sex == "masculino" || sex == "feminino")
-        {
-            validation = false; // Se o valor for válido
-        }
-    }
-
-    sex = textInfo.ToTitleCase(sex);
-    #endregion
-
-    #region "Variável 'age'"
-    validation = true;
-    double age = 0.0;
-    double age_months;
-    int age_years;
-    while (validation)
-    {
-        Console.Write("Idade: ");
-
-        // Correção da Exceção CS8600
-        var age_temp = Console.ReadLine();
-        #pragma warning disable CS8604 // Desabilitar alerta de uma referência possivelmente nula.
-        bool validator = calc.isConversable(age_temp);
-        age = calc.convertStringDouble(age_temp);
-        #pragma warning restore CS8604 // O próximo 'if' corrige este problema, mas o visual studio não reconhece.
-
-        if (age >= 1.0 && validator == true)
-            {
-                validation = false; // Se o valor for válido
-            }
-
-    }
-
-    age_years = (int)age;
-    age_months = (age - Math.Truncate(age)) * 12;
-
-    string age_str;
-    if (age_months > 0)
-    {
-        age_str = $"\nIdade: {age_years} anos e {age_months} meses";
-    }
-    else
-    {
-        age_str = $"\nIdade: {age_years} anos";
-    }
-    #endregion
-
-    #region "Variável 'height'"
-    validation = true;
-    double height = 0.0;
-    while (validation)
-    {
-        Console.Write("Altura (em centimetros [cm]): ");
-
-        // Correção da Exceção CS8600
-        var height_temp = Console.ReadLine();
-
-        #pragma warning disable CS8604 // Desabilitar alerta de uma referência possivelmente nula.
-        bool validator = calc.isConversable(height_temp);
-        height = calc.convertStringDouble(height_temp);
-        #pragma warning restore CS8604 // O próximo 'if' corrige este problema, mas o visual studio não reconhece.
-
-        if (height > 0 && validator == true)
-        {
-            validation = false; // Se o valor for válido
-        }
-    }
-    #endregion
-
-    #region "Variável 'weight'"
-    validation = true;
-    double weight = 0.0;
-    while (validation)
-    {
-        Console.Write("Peso (em quilogramas [Kg]): ");
-
-        // Correção da Exceção CS8600
-        var weight_temp = Console.ReadLine();
-        #pragma warning disable CS8604 // Desabilitar alerta de uma referência possivelmente nula.
-        bool validator = calc.isConversable(weight_temp);
-        weight = calc.convertStringDouble(weight_temp);
-        #pragma warning restore CS8604 // O próximo 'if' corrige este problema, mas o visual studio não reconhece.
-
-        if (weight > 0 && validator == true)
-        {
-            validation = false; // Se o valor for válido
-        }
-    }
-    #endregion
+    string sex = print.getSex();
 
     // Categoria por idade:
-    string category = calc.SelectCategory(age);
+    var age = print.getAge();
+    string category = calc.SelectCategory(age.Item1);
+    string age_str = print.getAgeString(age.Item1, age.Item2);
 
     // Cálculo do IMC e indicação da classificação do valor:
+    double height = print.getHeight();
+    double weight = print.getWeight();
+
     double imc = calc.CalculateIMC(weight, height);
     string classification = calc.IndicateClassification(imc);
 
@@ -149,21 +32,36 @@ static void Diagnostic()
     // Indicação das recomendações:
     string recommendation = calc.RecommendSolution(imc);
 
-    #region "Print diagnostic"
-    Console.WriteLine("\n***************************************************");
-    Console.WriteLine("DIAGNÓSTICO PRÉVIO");
-    Console.Write($"\nNome: {name}");
-    Console.Write($"\nSexo: {sex}");
-    Console.Write(age_str);
-    Console.Write($"\nAltura: {height/100:0.00} m");
-    Console.Write($"\nPeso: {weight} Kg");
-    Console.WriteLine($"\nCategoria: {category}");
-    Console.WriteLine($"\nIMC Desejável: entre 20 e 24");
-    Console.WriteLine($"\nResultado IMC: {imc:0.00} ({classification})");
-    Console.WriteLine($"\nRiscos: {risk}");
-    Console.WriteLine($"\nRecomendação inicial: {recommendation}");
-    Console.WriteLine("\n***************************************************");
+    #region "Confirmation"
+    bool validation = true;
+    string input_confirm = "";
+    while (validation)
+    {
+        Console.Write("\nDeseja alterar os dados? [S]/[N]");
+        var input_temp = Console.ReadLine();
+
+        // Correção da Exceção CS8600
+        if (input_temp is not null)
+        {
+            input_confirm = input_temp.ToLower();
+        }
+
+        if (input_confirm == "s" || input_confirm == "n")
+        {
+            validation = false; // Se o valor for válido
+        }
+
+        if (input_confirm == "s")
+        {
+            getDiagnostic();
+        }
+        else
+        {
+            print.printDiagnostic(name, sex, age_str, height, weight, category, imc, classification, risk, recommendation);
+
+        }
+    }
     #endregion
 }
 
-Diagnostic();
+getDiagnostic();
---
+++ IMCcalculatorLibraries/PrintLibrary.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace IMCcalculatorLibraries
+{
+    public class PrintLibrary
+    {
+        Calculator calc = new Calculator();
+        readonly TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+        public string getName()
+        {
+            bool validation = true;
+            string name = "";
+            while (validation)
+            {
+                Console.Write("Nome: ");
+                var name_temp = Console.ReadLine();
+
+                // Correção da Exceção CS8600
+                if (name_temp is not null && name_temp.Length > 0)
+                {
+                    name = name_temp;
+                    validation = false; // Se o valor for válido
+                }
+            }
+
+            name = textInfo.ToTitleCase(name.ToLower());
+            return name;
+        }
+
+        public string getSex()
+        {
+            bool validation = true;
+            string sex = "";
+            while (validation)
+            {
+                Console.Write("Sexo (Masculino ou Feminino): ");
+                var sex_temp = Console.ReadLine();
+
+                // Correção da Exceção CS8600
+                if (sex_temp is not null)
+                {
+                    sex = sex_temp.ToLower();
+                }
+
+                if (sex == "masculino" || sex == "feminino" || sex == "m" || sex == "f")
+                {
+                    validation = false; // Se o valor for válido
+                }
+            }
+
+            if (sex == "f")
+            {
+                sex = "Feminino";
+            }
+            else if (sex == "m")
+            {
+                sex = "Masculino";
+            }
+            
+            sex = textInfo.ToTitleCase(sex);
+            return sex;
+        }
+
+        public Tuple<int, int> getAge()
+        {
+            bool validation = true;
+            double age = 0.0;
+            int age_months;
+            int age_years;
+            while (validation)
+            {
+                Console.Write("Idade: ");
+
+                // Correção da Exceção CS8600
+                var age_temp = Console.ReadLine();
+                #pragma warning disable CS8604 // Desabilitar alerta de uma referência possivelmente nula.
+                bool validator = calc.isConversable(age_temp);
+                age = calc.convertStringDouble(age_temp);
+                #pragma warning restore CS8604 // O próximo 'if' corrige este problema, mas o visual studio não reconhece.
+
+                if (age >= 1.0 && validator == true)
+                {
+                    validation = false; // Se o valor for válido
+                }
+            }
+
+            age_years = (int)age;
+            age_months = (int)((age - Math.Truncate(age)) * 12);
+
+            return Tuple.Create(age_years, age_months);
+        }
+
+        public string getAgeString(int age_years, int age_months)
+        {
+            string age_str;
+            if (age_months > 0)
+            {
+                age_str = $"\nIdade: {age_years} anos e {age_months} meses";
+            }
+            else
+            {
+                age_str = $"\nIdade: {age_years} anos";
+            }
+
+            return age_str;
+        }
+
+        public double getHeight()
+        {
+            bool validation = true;
+            double height = 0.0;
+            while (validation)
+            {
+                Console.Write("Altura (em centimetros [cm]): ");
+
+                // Correção da Exceção CS8600
+                var height_temp = Console.ReadLine();
+
+                #pragma warning disable CS8604 // Desabilitar alerta de uma referência possivelmente nula.
+                bool validator = calc.isConversable(height_temp);
+                height = calc.convertStringDouble(height_temp);
+                #pragma warning restore CS8604 // O próximo 'if' corrige este problema, mas o visual studio não reconhece.
+
+                if (height > 25 && height < 230 && validator == true)
+                {
+                    validation = false; // Se o valor for válido
+                }
+            }
+
+            return height;
+        }
+
+        public double getWeight()
+        {
+            bool validation = true;
+            double weight = 0.0;
+            while (validation)
+            {
+                Console.Write("Peso (em quilogramas [Kg]): ");
+
+                // Correção da Exceção CS8600
+                var weight_temp = Console.ReadLine();
+                #pragma warning disable CS8604 // Desabilitar alerta de uma referência possivelmente nula.
+                bool validator = calc.isConversable(weight_temp);
+                weight = calc.convertStringDouble(weight_temp);
+                #pragma warning restore CS8604 // O próximo 'if' corrige este problema, mas o visual studio não reconhece.
+
+                if (weight > 5 && weight < 400 && validator == true)
+                {
+                    validation = false; // Se o valor for válido
+                }
+            }
+
+            return weight;
+        }
+
+        public void printDiagnostic(string name, string sex, string age_str, double height, double weight, string category, double imc, string classification, string risk, string recommendation)
+        {
+            Console.WriteLine("\n***************************************************");
+            Console.WriteLine("DIAGNÓSTICO PRÉVIO");
+            Console.Write($"\nNome: {name}");
+            Console.Write($"\nSexo: {sex}");
+            Console.Write(age_str);
+            Console.Write($"\nAltura: {height / 100:0.00} m");
+            Console.Write($"\nPeso: {weight} Kg");
+            Console.WriteLine($"\nCategoria: {category}");
+            Console.WriteLine($"\nIMC Desejável: entre 20 e 24");
+            Console.WriteLine($"\nResultado IMC: {imc:0.00} ({classification})");
+            Console.WriteLine($"\nRiscos: {risk}");
+            Console.WriteLine($"\nRecomendação inicial: {recommendation}");
+            Console.WriteLine("\n***************************************************");
+        }
+            
+    }
+}
